544cf56205a5af85dc1ac707d4e48fdf77107d04
Firdous <frdsbhat9@gmail.com>
2019-12-24 00:27:55 +0530

    3rd commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
-import './app_screens/first_screen.dart';
-
-
-void main() => runApp(MyFlutterApp());
-
-
-
-class MyFlutterApp extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: "My Flutter App",
-        home: Scaffold(
-          body: FirstScreen(),
-          appBar:AppBar(title: Text("My first App screen"),)
-        )
-    );
-
-  }
-
-
+import './app_screens/home.dart';
+void main(){
+  
+  runApp(MaterialApp(
+      title: "Exploring UI",
+      home: Home(),
+  ));
 }
